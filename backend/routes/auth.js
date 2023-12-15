57222e008f8bf5a546046a60c51fd82bf6d296c1
Deepak <b.deepakram@gmail.com>
2023-12-15 17:38:28 +0530

    encryption of passwords using salt and hashing + JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,10 @@ const express = require('express');
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-
+const JWT_SECRET = '$noopDawg';
 // CREATE a user using POST "/api/auth" doesn't require AUTH 
 router.post('/',
 [
@@ -23,21 +25,29 @@ router.post('/',
     if(user){
         return res.status(400).json({ error: "Sorry a user with this email already exists" })
     }
-  
+    const salt = await bcrypt.genSalt(10);
+    const securePass = await bcrypt.hash(req.body.password, salt);  //encrypts password
     user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: securePass,
         email: req.body.email,
       })
-      res.json(user)
+      const data = {
+        user:{
+          id: user.id
+        }
+      }
+      const authtoken = jwt.sign(data, JWT_SECRET);
+  
+  
+      // res.json(user)
+      res.json({authtoken})
     }
     catch(error){
         console.error(error.message);
         res.status(500).send("Some Error occured "+error);
 
     }
-
-
 
 })
 
