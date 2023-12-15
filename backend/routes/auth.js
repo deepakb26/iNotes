e18c8a9f7db412350d7fc88bf0bc4f9824f13812
Deepak <b.deepakram@gmail.com>
2023-12-15 17:58:39 +0530

    added login route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = '$noopDawg';
+
 // CREATE a user using POST "/api/auth" doesn't require AUTH 
 router.post('/',
 [
@@ -45,10 +46,50 @@ router.post('/',
     }
     catch(error){
         console.error(error.message);
-        res.status(500).send("Some Error occured "+error);
+        res.status(500).res.status(500).send("Internal Server Error"+error);
 
     }
 
 })
+
+// LOGIN with USER, requires authentication.
+router.post('/login', [ 
+    body('email', 'Enter a valid email').isEmail(),  //checks email format
+    body('password', 'Password cannot be blank').exists(),  //checks for empty vals
+  ], async (req, res) => {
+  
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  
+    const {email, password} = req.body;
+    try {
+      let user = await User.findOne({email});
+      if(!user){
+        return res.status(400).json({error: "Please try to login with correct credentials"});
+      }
+  
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if(!passwordCompare){
+        return res.status(400).json({error: "Please try to login with correct credentials"});
+      }
+  
+      const data = {
+        user:{
+          id: user.id
+        }
+      }
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({authtoken})
+  
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  
+  
+  })
 
 module.exports = router
