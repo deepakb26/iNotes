e8b411e7cca2c35042ed3386b186a3c769ebdb2b
Deepak <b.deepakram@gmail.com>
2023-12-19 14:27:22 +0530

    setup context API and added noteState and noteContext. Additionally frontend now displays defined notes

--- src/App.js
+++ src/App.js
@@ -7,16 +7,21 @@ import {
 import Navbar from './components/Navbar';
 import Home  from './components/Home';
 import About from './components/About';
+import NoteState from './context/notes/NoteState';
 function App() {
   return (
     <>
+    <NoteState>
     <Router>
       <Navbar /> 
+      <div className="container">
       <Routes>
         <Route exact path="/" element = {<Home/>}/>
         <Route exact path="/about" element={<About/>}/>
         </Routes>
+        </div>
     </Router>
+    </NoteState>
   </>
   );
 }
