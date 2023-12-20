c1257d991494b9626948534d944058585eccc3d0
Deepak <b.deepakram@gmail.com>
2023-12-20 13:42:15 +0530

    added addNote component and functionality ie create

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -69,8 +69,32 @@ const NoteState = (props) => {
     },
   ];
   const [notes, setNotes] = useState(notesInitial);
+  
+      // Add a Note
+      const addNote = (title, description, tag)=>{
+        console.log("Adding a new note")
+        const note = {
+          "_id": "61322f119553781a8ca8d0e21",
+          "user": "6131dc5e3e4037cd4734a0664",
+          "title": title,
+          "description": description,
+          "tag": tag,
+          "date": "2021-09-03T14:20:09.668Z",
+          "__v": 0
+        };
+        setNotes(notes.concat(note)) 
+      }
+
+      // Delete a Note
+      const deleteNote = ()=>{
+
+      }
+      // Edit a Note
+      const editNote = ()=>{
+
+      }
   return (
-    <noteContext.Provider value={{ notes, setNotes }}>
+    <noteContext.Provider value={{notes, addNote,deleteNote, editNote }}>
       {props.children}
     </noteContext.Provider>
   );
