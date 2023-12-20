cc3dd69e0eef890f50c381d45cbba833feb9d794
Deepak <b.deepakram@gmail.com>
2023-12-20 15:36:02 +0530

    deleteNote functionality implemented ie DELETE operation

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -5,7 +5,7 @@ import AddNote from './AddNote';
 
 const Notes = () => {
     const context = useContext(noteContext);
-    const {notes, addNote} = context;
+    const {notes} = context;
     return (
         <>
         <AddNote/>
@@ -16,7 +16,7 @@ const Notes = () => {
             })}
             </div>
             </>
-            
+
     )
 }
 
